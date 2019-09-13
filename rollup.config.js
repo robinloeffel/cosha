2cc5fbe9fd139d21780e67e70246ba0409b833f6
Robin Löffel <robin.loeffel@unic.com>
2019-09-13 15:12:03 +0200

    typo

--- rollup.config.js
+++ rollup.config.js
@@ -16,7 +16,7 @@ const copyConfig = {
 export default [{
   input: pkgjson.entry,
   output: {
-    file: pkgjson.browser,
+    file: pkgjson.unpkg,
     format: 'iife',
     name: 'Cosha'
   },
@@ -29,7 +29,7 @@ export default [{
 }, {
   input: pkgjson.entry,
   output: {
-    file: pkgjson.module,
+    file: pkgjson.main,
     format: 'esm'
   },
   plugins: [
