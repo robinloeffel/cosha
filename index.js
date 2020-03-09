2e5169d523f9fb96002189a843d70450fcfd2b6f
Robin Löffel <hi@robinloeffel.ch>
2020-03-09 09:40:23 +0100

    switch from getElementsByClassName to querySelectorAll

--- index.js
+++ index.js
@@ -6,46 +6,40 @@ const cosha = ({
     y = 0,
     x = 0
 } = {}) => {
-    const images = document.getElementsByClassName(className);
+    if ('filter' in document.documentElement.style) {
+        const images = document.querySelectorAll(`.${className}`);
 
-    if (!HTMLCollection.prototype.forEach) {
-        HTMLCollection.prototype.forEach = Array.prototype.forEach;
+        document.head.insertAdjacentHTML('beforeend', `
+            <style>
+                .${className}-wrapper {
+                    position: relative;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                }
+
+                .${className}-clone {
+                    filter: blur(${blur}) brightness(${brightness}) saturate(${saturation});
+                    position: absolute;
+                    z-index: -1;
+                    transform: translate3d(${x}, ${y}, 0);
+                }
+            </style>
+        `);
+
+        images.forEach(image => {
+            const clone = image.cloneNode();
+            const wrapper = document.createElement('div');
+
+            wrapper.classList.add(`${className}-wrapper`);
+            clone.classList.add(`${className}-clone`);
+            clone.classList.remove(className);
+
+            image.parentNode.insertBefore(wrapper, image);
+            wrapper.appendChild(image);
+            wrapper.appendChild(clone);
+        });
     }
-
-    if (!('filter' in document.documentElement.style)) {
-        return;
-    }
-
-    document.head.insertAdjacentHTML('beforeend', `
-        <style>
-            .${className}-wrapper {
-                position: relative;
-                display: flex;
-                align-items: center;
-                justify-content: center;
-            }
-
-            .${className}-clone {
-                filter: blur(${blur}) brightness(${brightness}) saturate(${saturation});
-                position: absolute;
-                z-index: -1;
-                transform: translate3d(${x}, ${y}, 0);
-            }
-        </style>
-    `);
-
-    images.forEach(image => {
-        const clone = image.cloneNode();
-        const wrapper = document.createElement('div');
-
-        wrapper.classList.add(`${className}-wrapper`);
-        clone.classList.add(`${className}-clone`);
-        clone.classList.remove(className);
-
-        image.parentNode.insertBefore(wrapper, image);
-        wrapper.appendChild(image);
-        wrapper.appendChild(clone);
-    });
 };
 
 export default cosha;
