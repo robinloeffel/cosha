85009b94aa217009690f08a9a51038eed587f0e5
Robin Löffel <hi@robinloeffel.ch>
2022-10-17 14:20:27 +0200

    chore: remove dist files from git

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare const _default: (className?: string, { blur, brightness, saturation, x, y }?: {
-    blur?: string;
-    brightness?: number | string;
-    saturation?: number | string;
-    x?: number | string;
-    y?: number | string;
-}) => void;
-export default _default;
